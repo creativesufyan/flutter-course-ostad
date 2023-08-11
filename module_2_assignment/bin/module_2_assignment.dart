df411c2d39f67dcb25de131528327c2e19caa181
Md. Abu Sufyan <122929887+creativesufyan@users.noreply.github.com>
2023-08-11 10:55:07 +0600

    Update module_2_assignment.dart

--- module_2_assignment/bin/module_2_assignment.dart
+++ module_2_assignment/bin/module_2_assignment.dart
@@ -1,18 +1,18 @@
 
 void main(){
-  List<int> marks = [85,92,78,65,88,72];    // Declare List of Marks
-  double sum = 0;
-  // Loop Statement to calculate sum of marks
-  for(int i= 0 ; i< marks.length; i++)
+  List<int> marks = [85,92,78,65,88,72];            // Declare List of Marks
+  double sum = 0;  
+                                                  
+  for(int i= 0 ; i< marks.length; i++)              // Loop Statement to calculate sum of marks
     {
       sum +=marks[i];
     }
-  double avgMarks = sum / marks.length; //Calculate avg marks of Student's in double Type
-  print("Student's average grade:$avgMarks"); //Print marks in double type
-  print("Rounded average:${avgMarks.toInt()}"); // Print Makrs in Rounded figure
-
-  // Conditional Statement for checking Pass or Fail
-  if(avgMarks >= 70)
+  
+  double avgMarks = sum / marks.length;             //Calculate avg marks of Student's in double Type
+  print("Student's average grade:$avgMarks");       //Print marks in double type
+  print("Rounded average:${avgMarks.toInt()}");     // Print Makrs in Rounded figure
+                                          
+  if(avgMarks >= 70)                               // Conditional Statement for checking Pass or Fail       
     {
       print("Passed");
     }
